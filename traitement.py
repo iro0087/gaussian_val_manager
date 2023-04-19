48e5e3c631623f1e76ff65ff93cf651c22e546b5
iro0087 <114911243+iro0087@users.noreply.github.com>
2023-04-20 00:03:32 +0200

    Update traitement.py

--- traitement.py
+++ traitement.py
@@ -6,7 +6,7 @@ import glob, os
 
 csv_file = str(input("What is the file?"))
 
-output_file = str(input("What is the name of the output file?"))
+output_file = str(input("What is the name of the output file (must be xlsx)?"))
 
 xl_file = csv_file + ".xlsx"
 
